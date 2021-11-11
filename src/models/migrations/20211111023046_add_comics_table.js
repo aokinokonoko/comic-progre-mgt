9781baf0198daa6726f445bd6b8f96cdd98e9dba
Aokinoko <aokinoko@AokinokonoiMac.local>
2021-11-11 11:44:10 +0900

    [ADD] make migration file.

--- src/models/migrations/20211111023046_add_comics_table.js
+++ src/models/migrations/20211111023046_add_comics_table.js
@@ -1,8 +1,33 @@
 
 exports.up = function(knex) {
+  return knex.schema.createTable("comics", (t) => {
+    // create the 'comis' table
+    t.increments() // auto-incrementing id column
+      .index(); // index this column
+
+    t.string("title")
+    .notNullable() // add a not-null constraint to this column
+
+    t.integer("volume")
+
+    t.string("author")
+    .notNullable() // add a not-null constraint to this column
+
+    t.string("publisher")
+
+    t.integer("pages")
+    .notNullable()
+
+    t.string("description")
+
+    t.timestamp("insert_at")
+      .notNullable()
+      .defaultTo(knex.fn.now()); // default to the current time
+  });
   
 };
 
 exports.down = function(knex) {
-  
+  // undo this migration by destroying the 'comics' table
+  return knex.schema.dropTable("user_messages");
 };
